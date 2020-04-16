62dc0f020c8ee0e2b129fcb87ccaa669c0bd64dc
Corentinver <56277702+Corentinver@users.noreply.github.com>
2020-04-16 10:56:10 +0200

    Update voiture.js

--- web/js/voiture.js
+++ web/js/voiture.js
@@ -1,13 +1,13 @@
 var voiture = [
 {
-	"id":0,
+	"id":1,
 	"nom":"Tesla model 3",
 	"puissance": "57",
 	"autonomie": "485000",
 	"puissanceMaximale":"170"
 },
 {
-	"id":1,
+	"id":2,
 	"nom":"Renault Zoe R75",
 	"puissance": "41",
 	"autonomie": "290000",
